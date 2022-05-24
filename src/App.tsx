681da374dad70436edbad733d5f3722c695c9504
Gabriel Sajfar <103059888+gabsaj@users.noreply.github.com>
2022-05-24 21:03:12 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,85 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "../src/styles/App.scss";
+import { Player } from "./types/Player";
 
 function App() {
+  const [players, setPlayers] = useState<Player[]>([]);
+
+  const playersList = [
+    {
+      id: 1,
+      name: "Name1",
+      nickname: " Nick1",
+    },
+    {
+      id: 2,
+      name: "Name2",
+      nickname: " Nick2",
+    },
+    {
+      id: 3,
+      name: "Name3",
+      nickname: " Nick3",
+    },
+    {
+      id: 1,
+      name: "Name1",
+      nickname: " Nick1",
+    },
+    {
+      id: 2,
+      name: "Name2",
+      nickname: " Nick2",
+    },
+    {
+      id: 3,
+      name: "Name3",
+      nickname: " Nick3",
+    },
+    {
+      id: 1,
+      name: "Name1",
+      nickname: " Nick1",
+    },
+    {
+      id: 2,
+      name: "Name2",
+      nickname: " Nick2",
+    },
+    {
+      id: 3,
+      name: "Name3",
+      nickname: " Nick3",
+    },
+    {
+      id: 1,
+      name: "Name1",
+      nickname: " Nick1",
+    },
+    {
+      id: 2,
+      name: "Name2",
+      nickname: " Nick2",
+    },
+    {
+      id: 3,
+      name: "Name3",
+      nickname: " Nick3",
+    },
+  ];
+
+  const fetchPlayers = () => {
+    setPlayers(playersList);
+  };
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+
   return (
     <div className="App">
       <div className="nav">
-        <div className="nav__logo">
-          <i className="logo-top"></i>
-          <i className="logo-bottom"></i>
-        </div>
+        <div className="logo">Big League</div>
         <div className="nav__pageName">Home</div>
         <div className="nav__add btn">Add player</div>
       </div>
@@ -16,20 +87,26 @@ function App() {
       <div className="main__layout">
         <div className="main__layout__title">featured players</div>
         <div className="filter">
+          <div className="sort">
+            Sort
+            <i className="icon--sort"></i>
+          </div>
           <div className="search">
             <i className="icon--search"></i>
             <input placeholder="Search..." className="input"></input>
           </div>
         </div>
         <div className="cards__container">
-          <div className="card">
-            <div className="card__imgContainer">
-              <div className="card__img"></div>
-              <div className="card__flag"></div>
+          {players.map((player) => (
+            <div className="card">
+              <div className="card__imgContainer">
+                <div className="card__img"></div>
+                <div className="card__flag"></div>
+              </div>
+              <div className="card__name">{player.name}</div>
+              <div className="card__role">{player.nickname}</div>
             </div>
-            <div className="card__name">Dodo</div>
-            <div className="card__role">Carrier</div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
